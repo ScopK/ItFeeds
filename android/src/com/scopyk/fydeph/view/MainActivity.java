7b449d6be2180488a29c380ea39ce13b0c3bace3
Sc-pyK <osc.car2@gmail.com>
2014-11-27 19:21:54 +0100

    Improved WebView

--- android/src/com/scopyk/fydeph/view/MainActivity.java
+++ android/src/com/scopyk/fydeph/view/MainActivity.java
@@ -15,11 +15,15 @@ import com.scopyk.fydeph.data.Content;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
 
@@ -29,6 +33,19 @@ public class MainActivity extends Activity implements APICallback {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        EditText pass = (EditText) findViewById(R.id.passfield);
+        pass.setOnEditorActionListener(new OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                	findViewById(R.id.goLogin).performClick();
+                }
+                return handled;
+            }
+        });
+        
         loginSetUp();
     }
     
--- android/src/com/scopyk/fydeph/view/ViewerActivity.java
+++ android/src/com/scopyk/fydeph/view/ViewerActivity.java
@@ -6,8 +6,13 @@ import com.scopyk.fydeph.data.*;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 
 public class ViewerActivity extends Activity {
@@ -24,7 +29,8 @@ public class ViewerActivity extends Activity {
         WebView wv = (WebView)findViewById(R.id.html_content);
         final String mimeType = "text/html";
         final String encoding = "UTF-8";
-        wv.loadDataWithBaseURL("", post.getDescription(), mimeType, encoding, "");
+        final String style = "<style>body{font-size:3em;}p,div,img{width:100%;background-color:#fff;}table{width:100wv;}table img{width:initial}</style>";
+        wv.loadDataWithBaseURL("", style+post.getDescription(), mimeType, encoding, "");
         
         WebSettings ws = wv.getSettings();
         wv.setHorizontalScrollBarEnabled(false);
@@ -35,6 +41,13 @@ public class ViewerActivity extends Activity {
         ws.setSupportZoom(true); 
         ws.setLoadWithOverviewMode(true);
         ws.setDisplayZoomControls(false);
+        
+        wv.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+            	//Toast.makeText(ViewerActivity.this.getApplicationContext(), newProgress+"", 1).show();
+            }
+        });
     }
-
 }
