07cc3b9f079a8cb01eef3d5413c46c91a7141a80
Sc-pyK <osc.car2@gmail.com>
2015-10-11 14:29:06 +0200

    Top-message removed
    Need to update calls yet

--- scripts/lib/messages.js
+++ scripts/lib/messages.js
@@ -4,6 +4,7 @@
 	css += ".message_sys{\n";
 	css += "	font-family:'Noto Sans','Lucida Sans Unicode', 'Lucida Grande', sans-serif;\n";
 	css += "}\n";
+	/*
 	css += "#top_message{\n";
 	css += "	z-index:30;\n";
 	css += "	position:fixed;\n";
@@ -25,6 +26,12 @@
 	css += "	font-size: 13.5px;\n";
 	css += "	margin: 0.6em 0px;\n";
 	css += "}\n";
+	/**/
+	css += "#top_message.hidden,#popup_message.hidden{\n";
+	css += "	transition-delay: 0s, 0.3s;\n";
+	css += "	right:-100px;\n";
+	css += "	opacity:0; visibility:hidden;\n";
+	css += "}\n";
 	css += "#popup_message{\n";
 	css += "	z-index:30;\n";
 	css += "	position:fixed;\n";
@@ -35,12 +42,9 @@
 	css += "	color: black;\n";
 	css += "	opacity:1; visibility:visible;\n";
 	css += "	box-shadow: 0px 0px 6px -2px;\n";
+	css += "	border:solid #444;\n";
+	css += "	border-width:1px 0 1px 1px;\n";
 	css += "	transition: all 0.3s, visibility 0s 0s;\n";
-	css += "}\n";
-	css += "#popup_message.hidden{\n";
-	css += "	transition-delay: 0s, 0.3s;\n";
-	css += "	right:-100px;\n";
-	css += "	opacity:0; visibility:hidden;\n";
 	css += "}\n";
 	css += "#popup_message p{\n";
 	css += "	font-variant: small-caps;\n";
@@ -51,17 +55,27 @@
 	css += "	margin:0; padding: 10px 25px;\n";
 	css += "}\n";
 
+	css += "#popup_message.good{\n";
+	css += "	border-color: #0C7B19;\n";
+	css += "	background-color: #60BC38;\n";
+	css += "}\n";
+
+	css += "#popup_message.bad{\n";
+	css += "	border-color: #7B0C0C;\n";
+	css += "	background-color: #C83838;\n";
+	css += "}\n";	
+
 	styl.innerHTML = css;
 	document.head.appendChild(styl);
 })();
 
 window.addEventListener("load", function() {
-    var di = document.createElement('div');
-    di.id = 'top_message';
-    di.className ="message_sys";
-    di.onmouseover = function(){hideMessage()};
-	di.appendChild(document.createElement('p'));
-    document.body.appendChild(di);
+    //var di = document.createElement('div');
+    //di.id = 'top_message';
+    //di.className ="message_sys";
+    //di.onmouseover = function(){hideMessage()};
+	//di.appendChild(document.createElement('p'));
+    //document.body.appendChild(di);
 
     var di = document.createElement('div');
     di.id = 'popup_message';
@@ -71,41 +85,33 @@ window.addEventListener("load", function() {
     document.body.appendChild(di);
 });
 
-
-var hidemsgtimer;
-function showMessage(msg, good){
-	msg = (typeof msg !== 'undefined')? msg : "";
-	good = (typeof good !== 'undefined')? good : false;
-
-	var box = document.getElementById("top_message");
-	if (good)
-		box.className = "message_sys good";
-	else
-		box.className = "message_sys";
-	box.children[0].innerHTML=msg;
-	box.style.top=0;
-	
-	clearTimeout(hidemsgtimer);
-	hidemsgtimer = setTimeout(hideMessage,3000);
-}
-function hideMessage() {
-	var hh = document.getElementById("top_message").offsetHeight;
-	document.getElementById("top_message").style.top="-"+(hh+6)+"px";
-}
-
 var hidepopmsgtimer;
-function showPopMessage(msg){
+function showPopMessage(msg,good){
+	var classes;
+	if (good==undefined) 	classes= "message_sys";
+	else if (good)			classes= "message_sys good";
+	else					classes= "message_sys bad";
+
 	msg = (typeof msg !== 'undefined')? msg : "";
 
 	var box = document.getElementById("popup_message");
 	box.children[0].innerHTML=msg;
 	box.style.display="";
-	box.className = "message_sys";
+	box.className = classes;
 
 	clearTimeout(hidepopmsgtimer);
 	hidepopmsgtimer = setTimeout(hidePopMessage,2000);
 }
 
 function hidePopMessage(){
-	document.getElementById("popup_message").className = "message_sys hidden";
+	document.getElementById("popup_message").className += " hidden";
+}
+
+function showMessage(msg, good){
+	good = good==undefined?false:good;
+	showPopMessage(msg,good);
+}
+function hideMessage() {
+	alert();
+	hidePopMessage();
 }
