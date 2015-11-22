68c4b43801ecec6948ddb260fdf8d783cfce858b
Scop <osc.car2@gmail.com>
2015-11-22 11:09:43 +0100

    Removing old android project
    Switching from Android Eclipse to Android Studio

--- android/src/com/scopyk/fydeph/APICallback.java
+++
@@ -1,8 +0,0 @@
-package com.scopyk.fydeph;
-
-import org.json.JSONException;
-
-public interface APICallback {
-	public void APIResponse(org.json.JSONObject json, int id, APICall parent) throws JSONException;
-	public android.content.Context getApplicationContext();
-}
--- android/src/com/scopyk/fydeph/data/MenuLabel.java
+++
@@ -1,7 +0,0 @@
-package com.scopyk.fydeph.data;
-
-public interface MenuLabel {
-	public String getLabel();
-	public String getTitle();
-	public String getId();
-}
